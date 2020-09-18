e9d8c07b32f7ecced4010dd65dc15c8fb2ace2e2
Isaac Rios <jirb1213@live.com.mx>
2020-09-18 16:28:17 -0500

    Se quito la seccion del boton y se agregaron los estilos en el Header

--- src/utils/buttonClick.js
+++
@@ -1,8 +0,0 @@
-const $createButton = document.getElementById("button");
-const $contentUp = document.getElementById("contentUp");
-// const $contentDown = document.getElementById("contentDown");
-
-$createButton.addEventListener("click",()=>{
-    $contentUp.classList.toggle("up");
-    // $contentDown.classList.toggle("down");
-})
